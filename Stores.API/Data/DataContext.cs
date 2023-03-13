bb5e297e3679657e107c9b0251f2c6fafe9e792c
evelynatehortua <lilinprisco@gmail.com>
2023-03-12 22:10:53 -0500

    Commit Country-DataContext

---
+++ Stores.API/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stores.Shared.Entities;
+
+namespace Stores.API.Data
+{
+    // Donde nos vamos a conectar con la base de datos
+    public class DataContext : DbContext
+    {
+        // Constructor
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
---
+++ Stores.Shared/Entities/Country.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stores.Shared.Entities
+{
+    public class Country
+    {
+        // Para la base de datos
+        public int Id { get; set; }
+        [Display(Name = "País")]
+        [MaxLength(100, ErrorMessage = "Cuidado, el campo {0} solo puede contener {1} caracteres")]
+        [Required(ErrorMessage = "* El campo {0} es obligatorio")]
+        public string Name { get; set; } = null!;
+
+
+        
+    }
+}
